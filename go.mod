953b5cc9bbb9d8fb31e3304776f11cce42adada2
CC11001100 <CC11001100@qq.com>
2023-01-10 02:36:24 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/golang-infrastructure/go-rail-fence-cipher
+
+go 1.18
---
+++ rail_fence_cipher.go
@@ -0,0 +1,10 @@
+package rail_fence_cipher
+
+// Encrypt 对文本进行加密
+func Encrypt(plaintext string, columns int) string {
+	return ""
+}
+
+func Decrypt(encryptText string) string {
+	return ""
+}
